b190e1993bd1bb09276d9f9576dc4d2aad240536
namprokash <onamprokashe@gmail.com>
2026-01-03 16:43:53 +0400

    Final commit before push

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongoUrl = "mongodb://localhost:27017/iNoteBook";
+const mongoUrl =
+  "mongodb+srv://msia:Saif7799568@cluster0.nss7iq3.mongodb.net/iNotebook";
 
 const connectMongo = async () => {
   await mongoose.connect(mongoUrl);
